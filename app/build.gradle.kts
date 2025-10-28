2a63dd6175b688c09aaf30fc25cf22a1d5eba28d
Nathan Campos <nathan@innoveworkshop.com>
2025-10-28 14:29:58 +0000

    Added libraries for HTTP and JSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.gson)
+    implementation(libs.fuel)
+    runtimeOnly(libs.fuel.android)
+    runtimeOnly(libs.fuel.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
